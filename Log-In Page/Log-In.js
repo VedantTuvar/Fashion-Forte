ad3d100039c6614cd6dac529a03fdaddf1e61819
Vedant Tuvar <152903617+VedantTuvar@users.noreply.github.com>
2024-08-01 13:59:09 +0530

    Eye-Icon Completed in Login Page

--- Log-In Page/Log-In.js
+++ Log-In Page/Log-In.js
@@ -9,3 +9,20 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+const togglePassword = (toggleId, passwordId) => {
+  const toggleIcon = document.querySelector(toggleId);
+  const passwordField = document.querySelector(passwordId);
+
+  toggleIcon.addEventListener("click", () => {
+    const type =
+      passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+
+    toggleIcon.classList.toggle("fa-eye");
+    toggleIcon.classList.toggle("fa-eye-slash");
+  });
+};
+
+togglePassword("#toggle-login-password", "#login-password");
+togglePassword("#toggle-signup-password", "#signup-password");
